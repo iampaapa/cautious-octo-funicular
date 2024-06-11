f2a14d08201a88f245319cd7777af6f40f30c639
Rudy Ayitinya Sulley <78674026+ayitinya@users.noreply.github.com>
2024-06-11 07:09:40 +0000

    chore: fix type errors in file

--- src/data/settings.ts
+++ src/data/settings.ts
@@ -3,106 +3,107 @@ import type { QuizSettings } from '@/features/quiz/types'
 export const STANDARD_NSMQ_SETTINGS: QuizSettings = {
   id: 'standard-nsmq',
   name: 'Standard NSMQ',
+  playerSetup: 'single',
   rounds: {
     roundOne: {
       fields: [
         {
           name: 'Biology',
-          questionCount: 4
+          questionCount: 4,
         },
         {
           name: 'Physics',
-          questionCount: 4
+          questionCount: 4,
         },
         {
           name: 'Mathematics',
-          questionCount: 4
+          questionCount: 4,
         },
         {
           name: 'Chemistry',
-          questionCount: 4
-        }
-      ]
+          questionCount: 4,
+        },
+      ],
     },
     roundTwo: {
       fields: [
         {
           name: 'Biology',
-          questionCount: 4
+          questionCount: 4,
         },
         {
           name: 'Physics',
-          questionCount: 4
+          questionCount: 4,
         },
         {
           name: 'Mathematics',
-          questionCount: 4
+          questionCount: 4,
         },
         {
           name: 'Chemistry',
-          questionCount: 4
-        }
-      ]
+          questionCount: 4,
+        },
+      ],
     },
     roundThree: {
       fields: [
         {
           name: 'Biology',
-          questionCount: 4
+          questionCount: 4,
         },
         {
           name: 'Physics',
-          questionCount: 4
+          questionCount: 4,
         },
         {
           name: 'Mathematics',
-          questionCount: 4
+          questionCount: 4,
         },
         {
           name: 'Chemistry',
-          questionCount: 4
-        }
-      ]
+          questionCount: 4,
+        },
+      ],
     },
     roundFour: {
       fields: [
         {
           name: 'Biology',
-          questionCount: 4
+          questionCount: 4,
         },
         {
           name: 'Physics',
-          questionCount: 4
+          questionCount: 4,
         },
         {
           name: 'Mathematics',
-          questionCount: 4
+          questionCount: 4,
         },
         {
           name: 'Chemistry',
-          questionCount: 4
-        }
-      ]
+          questionCount: 4,
+        },
+      ],
     },
     roundFive: {
       fields: [
         {
           name: 'Biology',
-          questionCount: 4
+          questionCount: 4,
         },
         {
           name: 'Physics',
-          questionCount: 4
+          questionCount: 4,
         },
         {
           name: 'Mathematics',
-          questionCount: 4
+          questionCount: 4,
         },
         {
           name: 'Chemistry',
-          questionCount: 4
-        }
-      ]
-    }
+          questionCount: 4,
+        },
+      ],
+    },
   },
 }
