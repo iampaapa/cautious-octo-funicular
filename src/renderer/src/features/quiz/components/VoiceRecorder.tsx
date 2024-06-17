ea6f6aa17d94164cf47bceec7c654fe79f73a660
Rudy Ayitinya Sulley <78674026+ayitinya@users.noreply.github.com>
2024-06-17 16:38:07 +0000

    feat: add audio and recorder visual animations

---
+++ src/renderer/src/features/quiz/components/VoiceRecorder.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../assets/voice_recorder.scss'
+import { faMicrophone } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const VoiceRecorder: React.FC<{ animate: boolean; onClick: () => void }> = ({
+  animate,
+  onClick
+}) => {
+  return (
+    <>
+      <div className={`mic-container `} onClick={onClick}>
+        <div className={`circle ${animate ? 'active' : ''}`}>
+          <FontAwesomeIcon className="i" icon={faMicrophone} />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default VoiceRecorder
